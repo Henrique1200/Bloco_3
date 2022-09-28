4bdd22c4112b401aba2be334d443232c6447cc29
Luiz Martins <henriquedelbo@gmail.com>
2022-09-28 00:39:08 -0300

    javaScript

---
+++ Fundamentos-Web-main/Fundamentos-Web-main/Formulario/assets/script.js
@@ -0,0 +1,8 @@
+/*
+Case Sensitive = reconhece letras maiusculas e minusculas 
+ por Tag: getElementByTagName()
+ por Id: getElementById()
+ por Nome: getElementsByName()
+ por Classe: getElementsByClassName()
+ por Seletor: querySelector()
+ */
